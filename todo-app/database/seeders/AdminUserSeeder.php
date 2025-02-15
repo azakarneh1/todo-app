e2c4a91bd92d85851c92aceded5be09072a740ad
zak <zakarneh21@hotmail.comn>
2025-02-15 20:36:46 +0300

    admin creation seeder added

---
+++ todo-app/database/seeders/AdminUserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@mail.com'],
+            [
+                'name' => 'admin',
+                'email' => 'admin@mail.com',
+                'password' => Hash::make('123456789'),
+                'role' => 'admin',
+            ]
+        );
+    }
+}
